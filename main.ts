0c06ba40f7636720d55c7637bb2efe65a5f222f4
thecodeguild <73739114+thecodeguild@users.noreply.github.com>
2022-01-23 13:34:07 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,31 @@
 let direction = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showString("" + (input.compassHeading()))
-})
-input.onButtonPressed(Button.B, function () {
-	
+    basic.showNumber(input.compassHeading())
 })
 basic.forever(function () {
     direction = input.compassHeading()
-    if (direction == 0) {
+    if (direction > 337.5 && direction < 22.5) {
         basic.showString("N")
     }
-    if (direction == 90) {
+    if (direction > 22.5 && direction < 67.5) {
+        basic.showString("NE")
+    }
+    if (direction > 67.5 && direction < 112.5) {
         basic.showString("E")
     }
-    if (direction == 180) {
+    if (direction > 112.5 && direction < 157.5) {
+        basic.showString("SE")
+    }
+    if (direction > 157.5 && direction < 202.5) {
         basic.showString("S")
     }
-    if (direction == 270) {
+    if (direction > 202.5 && direction < 247.5) {
+        basic.showString("SW")
+    }
+    if (direction > 247.5 && direction < 292.5) {
         basic.showString("W")
+    }
+    if (direction > 292.5 && direction < 337.5) {
+        basic.showString("NW")
     }
 })
